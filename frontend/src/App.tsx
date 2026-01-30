c8721c1a9fcadfb8384a778e11208d1cff3aa3c9
Pai314 <pai314.eequoalmc2@gmail.com>
2026-01-30 19:05:48 +0900

    Fix imports and verify build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Layout from './components/Layout';
-
-
-import Dashboard from './pages/Dashboard';
-import CourseDetail from './pages/CourseDetail';
-import CourseCreate from './pages/CourseCreate';
-import CalendarPage from './pages/CalendarPage';
-
-
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Layout from './components/Layout';
-
-
 import Dashboard from './pages/Dashboard';
 import CourseDetail from './pages/CourseDetail';
 import CourseCreate from './pages/CourseCreate';
